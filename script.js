19f206cdc26bb4dc17c13eb4f64f3fa3fddc962f
Glitch (glitch-in-bio) <none>
2023-09-29 14:21:12 +0000

    🎏🛀 Checkpoint ./index.html:30572456/6952 ./script.js:30572456/12278 ./styles.css:30572456/10189

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// JavaScript for fetching IP address and location
+window.addEventListener('load', () => {
+    // Fetch IP address and location
+    fetch('https://ipinfo.io/json')
+        .then((response) => response.json())
+        .then((data) => {
+            const ipAddress = data.ip;
+            const location = `${data.city}, ${data.region}, ${data.country}`;
+            
+            document.getElementById('ip-address').textContent = ipAddress;
+            document.getElementById('location').textContent = location;
+        })
+        .catch((error) => {
+            console.error('Error fetching IP and location data:', error);
+            document.getElementById('ip-address').textContent = 'Unable to fetch IP address';
+            document.getElementById('location').textContent = 'Unable to fetch location';
+        });
+});
+
 document.getElementById('dark-mode-toggle').addEventListener('change', () => {
     document.body.classList.toggle('dark-mode');
 });
