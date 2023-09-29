1920b1738703e566a3be86fd0025203d7c888859
Glitch (glitch-in-bio) <none>
2023-09-29 14:33:01 +0000

    ✌️👸 Checkpoint ./index.html:30572456/5853 ./script.js:30572456/13444 ./styles.css:30572456/13038

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// JavaScript for fetching IP address and location
+// JavaScript for fetching IPv4 and IPv6 addresses and location
 window.addEventListener('load', () => {
-    // Fetch IP address and location
+    // Fetch IPv4 and IPv6 addresses and location using ipinfo.io
     fetch('https://ipinfo.io/json')
         .then((response) => response.json())
         .then((data) => {
-            const ipAddress = data.ip;
+            const ipv4Address = data.ip;
+            const ipv6Address = data.ip6 || 'N/A';
             const location = `${data.city}, ${data.region}, ${data.country}`;
             
-            document.getElementById('ip-address').textContent = ipAddress;
+            document.getElementById('ipv4-address').textContent = ipv4Address;
+            document.getElementById('ipv6-address').textContent = ipv6Address;
             document.getElementById('location').textContent = location;
         })
         .catch((error) => {
             console.error('Error fetching IP and location data:', error);
-            document.getElementById('ip-address').textContent = 'Unable to fetch IP address';
+            document.getElementById('ipv4-address').textContent = 'Unable to fetch IPv4 address';
+            document.getElementById('ipv6-address').textContent = 'Unable to fetch IPv6 address';
             document.getElementById('location').textContent = 'Unable to fetch location';
         });
 });
